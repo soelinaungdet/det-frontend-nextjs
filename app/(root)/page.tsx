eea0912653fa50100be2b41e9adfdd81056b36d1
soelinaungdet <soelinaung.det@gmail.com>
2024-12-14 10:17:55 +0630

    ui.shadcn tailwind added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <Button onClick={() => setOpen(true)}>Click Me</Button>
+      <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Hello World!</DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
